d533de767d7d1da83805d7c3ace7e4a0085909be
UAPROGRAMER <stasyatskiu2008@gmail.com>
2024-07-05 08:48:18 +0000

    modified:   stdlib.asm

--- stdlib.asm
+++ stdlib.asm
@@ -1,4 +1,6 @@
 section .data
+    stdlibtext db "This program made using stdlib.asm library v1.7.5 by https://github.com/UAPROGRAMER."
+
     newline db 10, 0
     getArgvErrorIndexOutOfRange db "<ERROR>:(getArgvErrorIndexOutOfRange)", 10, 0
 
@@ -9,6 +11,25 @@ section .bss
 
     argc resb 8
     argv resb 8
+
+SYS_EXIT equ 60
+SYS_WRITE equ 1
+SYS_READ equ 0
+SYS_OPEN equ 2
+SYS_CLOSE equ 3
+
+STD_IN equ 0
+STD_OUT equ 1
+STD_ERROR equ 2
+
+OPENMODE_READ equ 0
+OPENMODE_WRITE equ 1
+OPENMODE_RDWR equ 2
+OPENMODE_CREATE equ 64
+OPENMODE_APPEND equ 1024
+OPENMODE_DIRECTORY equ 65536
+OPENMODE_PATH equ 2097152
+OPENMODE_TMPFILE equ 4194394
 
 %macro print 1
     mov rax, %1
@@ -22,8 +43,8 @@ section .bss
     inc rax
     jmp %%printLoop
 %%endPrintLoop:
-    mov rax, 1
-    mov rdi, 1
+    mov rax, SYS_WRITE
+    mov rdi, STD_OUT
     mov rsi, [printSpace]
     mov rdx, rbx
     syscall
@@ -57,8 +78,8 @@ section .bss
 %%printRAXLoop2:
     mov rcx, [digitSpacePos]
  
-    mov rax, 1
-    mov rdi, 1
+    mov rax, SYS_WRITE
+    mov rdi, STD_OUT
     mov rsi, rcx
     mov rdx, 1
     syscall
@@ -84,15 +105,15 @@ section .bss
     inc rax
     jmp %%printLoop
 %%endPrintLoop:
-    mov rax, 1
-    mov rdi, 2
+    mov rax, SYS_WRITE
+    mov rdi, STD_ERROR
     mov rsi, [printSpace]
     mov rdx, rbx
     syscall
 %endmacro
 
 %macro exit 1
-    mov rax, 60
+    mov rax, SYS_EXIT
     mov rdi, %1
     syscall
 %endmacro
@@ -130,4 +151,37 @@ section .bss
     exit 1
 
     %%getArgvEnd:
+%endmacro
+
+%macro write 2
+    mov rax, %1
+    mov [printSpace], rax
+    mov rbx, 0
+%%printLoop:
+    mov cl, [rax]
+    cmp cl, 0
+    je %%endPrintLoop
+    inc rbx
+    inc rax
+    jmp %%printLoop
+%%endPrintLoop:
+    mov rax, SYS_WRITE
+    mov rdi, %2
+    mov rsi, [printSpace]
+    mov rdx, rbx
+    syscall
+%endmacro
+
+%macro open 3
+    mov rax, SYS_OPEN
+    mov rdi, %1
+    mov rsi, %2
+    mov rdx, %3
+    syscall
+%endmacro
+
+%macro close 1
+    mov rax, SYS_CLOSE
+    mov rdi, %1
+    syscall
 %endmacro
